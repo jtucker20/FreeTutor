f80ea9456de8b11e3ac4f2f5d61a4a53af6f4807
Jeremiah Tucker <jtucker@mcdaniel.edu>
2019-11-17 14:05:32 -0500

    Finished commenting test file

--- src/test/java/edu/mcdaniel/java2206/template/replacewithprojectname/ApplicationTests.java
+++ src/test/java/edu/mcdaniel/java2206/template/replacewithprojectname/ApplicationTests.java
@@ -6,9 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
+
+
 	@Test
 	void contextLoads() {
-		//Here we need to implement testing!
+		//We are testing to see if the spring boot application loads.  This will fail only if the spring boot
+		// application fails to load. No need to add functionality here.
 	}
 
 }
